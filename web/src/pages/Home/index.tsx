65339af1fb751c192664a39ff6ee0e847bc184b8
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-04 16:02:22 -0300

    Develop createPoint page html and css

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import './styles.css';
 // import { Container } from './styles';
 
@@ -16,12 +17,12 @@ const Home: React.FC = () => {
           <p>
             Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente
           </p>
-          <a href='/cadastro'>
+          <Link to='/create-point'>
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
