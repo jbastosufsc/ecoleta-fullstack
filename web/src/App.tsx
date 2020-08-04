27dba915296d08c4ec028776deaa45f456c1aa38
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-04 14:28:46 -0300

    Create Home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './App.css';
+
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>hello ecoleta</h1>
+        <Home />
       </header>
     </div>
   );
